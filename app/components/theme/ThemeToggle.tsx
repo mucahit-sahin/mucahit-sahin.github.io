c30d6daf6bcb10b7e26dde75971921656453f59c
mucahitsahin <mucahitsahin786@gmail.com>
2024-12-06 16:21:58 +0300

    add theme provider and toggle components for dark mode support

---
+++ app/components/theme/ThemeToggle.tsx
@@ -0,0 +1,20 @@
+import { useTheme } from "next-themes";
+import { IoMdMoon, IoMdSunny } from "react-icons/io";
+
+const ThemeToggle = () => {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <div className="h-9 w-9 flex justify-center rounded-full md:p-0 ml-2 ">
+      <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+        {theme === "dark" ? (
+          <IoMdSunny className="h-8 w-8 text-orange-400" />
+        ) : (
+          <IoMdMoon className="h-6 w-6 rounded-full text-black" />
+        )}
+      </button>
+    </div>
+  );
+};
+
+export default ThemeToggle;
