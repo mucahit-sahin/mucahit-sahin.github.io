76bad565400e8b51ab1255543cbf40e0067af2c6
mucahitsahin <mucahitsahin786@gmail.com>
2024-12-06 19:55:00 +0300

    add Profile component and integrate it into the homepage layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
+import Profile from "./components/Profile";
 import Sidebar from "./components/Sidebar";
 
 export default function Home() {
   return (
-    <div className="flex flex-row h-screen lg:max-w-7xl md:max-w-xl mx-auto px-2  sm:px-0 ">
-      <Sidebar />
-      <div className="flex-[1_1_75%] flex flex-row bg-white ">
-        <h1 className="w-full">Main</h1>
+    <div className="bg-white dark:bg-black">
+      <div className="flex flex-row h-screen lg:max-w-7xl md:max-w-xl mx-auto px-2  sm:px-0 ">
+        <Sidebar />
+        <Profile />
+        <div className="flex-[1_1_25%] flex flex-row dark:bg-gray-700 ">
+          <h1 className="w-full">Den</h1>
+        </div>
       </div>
     </div>
   );
