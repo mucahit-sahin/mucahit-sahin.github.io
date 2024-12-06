630b1a86b45548fbc4ee68032b7c2a08dfb653d9
mucahitsahin <mucahitsahin786@gmail.com>
2024-12-06 19:54:37 +0300

    update Sidebar and SidebarItem components for improved icon usage and active state handling

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,15 +1,17 @@
+"use client";
 import SidebarItem from "./SidebarItem";
 import { FaUser, FaGithub } from "react-icons/fa";
 import { GrProjects } from "react-icons/gr";
 import { MdContactMail, MdBookmarks } from "react-icons/md";
+import { BsPostcard } from "react-icons/bs";
 
 export default function Sidebar() {
   return (
-    <div className="flex-[1_1_25%] px-5 flex flex-col min-h-screen ">
+    <div className="flex-[1_1_25%] px-5 flex flex-col min-h-screen">
       <FaGithub size={30} className="mt-3 ml-5 mb-5" />
       <SidebarItem href="/" text="Profile" Icon={FaUser} />
       <SidebarItem href="/projects" text="Projects" Icon={GrProjects} />
-      <SidebarItem href="/posts" text="Posts" Icon={FaUser} />
+      <SidebarItem href="/posts" text="Posts" Icon={BsPostcard} />
       <SidebarItem href="/contact" text="Contact" Icon={MdContactMail} />
       <SidebarItem href="/bookmarks" text="Bookmarks" Icon={MdBookmarks} />
 
@@ -25,7 +27,7 @@ export default function Sidebar() {
         </div>
         <div className="flex flex-col ml-2 mr-12">
           <div className="text-[#d9d9d9] text-sm font-bold leading-[19.6875px]">
-            <span>Mücahit Şahin</span>
+            <span className="text-black dark:text-white">Mücahit Şahin</span>
           </div>
           <div className="text-[#6e767d] text-sm leading-[19.6875px]">
             <span>@mucahit-sahin</span>
--- app/components/SidebarItem.tsx
+++ app/components/SidebarItem.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 function SidebarItem({
   text,
@@ -9,23 +12,26 @@ function SidebarItem({
   href: string;
   Icon: React.ElementType;
 }) {
-  const active = false;
+  const pathname = usePathname();
+  const active = pathname === href;
+
   return (
     <Link
       href={href}
       className={`flex items-center w-min ${
         active ? "rounded-full text-blue-500 hover:text-white" : ""
-      }  hover:rounded-full hover:bg-blue-600`}
+      } hover:rounded-full hover:bg-blue-600`}
     >
       <Icon size={24} className="m-4" />
       <span
         className={`mr-5 ml-auto text-lg font-bold leading-6 ${
-          active ? "text-blue-500" : "text-white"
-        } `}
+          active ? "text-blue-500" : "text-black dark:text-white"
+        }`}
       >
         {text}
       </span>
     </Link>
   );
 }
+
 export default SidebarItem;
